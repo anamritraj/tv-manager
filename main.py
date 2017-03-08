f61138cc360641db45f56b674dc53b65ebf7c135
anamritraj <anandamritraj904@gmail.com>
2017-03-08 12:48:33 +0530

    Inital commit. Add TODOs.

---
+++ main.py
@@ -0,0 +1,17 @@
+
+"""
+    TODO:
+    - Manage TV files: Rename, create folders according to name, season, episode etc.
+    - Keep track of what has been watched.
+    - Keep track of what to watch next.
+    - Keep track of which episode airs next and remind to watch it.
+    - Get the latest episode from the internet, automatically.
+    - Notifications on both phone and PC using Twilio and native notifications API.
+"""
+
+
+def main():
+    print("TV Manager")
+
+if __name__ == '__main__':
+    main()
