a7d4dca74f09e8c1102d328a13e15c5028186d7a
anamritraj <anandamritraj904@gmail.com>
2017-03-10 20:46:22 +0530

    Get list of files from a folder, Accessing recurcively.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 """
     TODO:
     - Manage TV files: Rename, create folders according to name, season, episode etc.
@@ -8,10 +7,35 @@
     - Get the latest episode from the internet, automatically.
     - Notifications on both phone and PC using Twilio and native notifications API.
 """
+from glob import glob
+import pprint
+import os
+
+EXTENSIONS = [".mp4", ".mkv"]
+
+# PATH = "C:/Users/aaa/Desktop/TODO"
+PATH = "D:/TV"
+
+
+def get_tv_files(PATH):
+    """
+    Return all the files in a directory, recurcively.
+    :param PATH: Path to a dirty folder containing unorganized files.
+    :return: List of files in the folder.
+    """
+    files = []
+    for folderName, subfolders, filenames in os.walk(PATH):
+        print("Scanning " + folderName)
+        for file in filenames:
+            for ext in EXTENSIONS:
+                if file.endswith(ext):
+                    files.append(file)
+    return files
 
 
 def main():
-    print("TV Manager")
+    files = get_tv_files(PATH)
+    pprint.pprint(files)
 
 if __name__ == '__main__':
     main()
